722b0aa1a2d5f8c7bedfe6b8b7ff4e6b125d6b34
Francine <francinepereira467@gmail.com>
2025-06-21 01:22:54 -0300

    deixado conexão com banco em memória por padrão

--- backend/DB/conn.js
+++ backend/DB/conn.js
@@ -1,17 +1,26 @@
+
 const { Sequelize } = require("sequelize");
 const pg = require("pg");
 
-const conexao = new Sequelize(process.env.DATABASE_URL, {
-  host: "localhost",
-  dialect: "postgres",
-  timezone: "-03:00", // Fuso horário de Brasília (sem horário de verão)
-  dialectOptions: {
-    useUTC: false, // Evita usar UTC no PostgreSQL
-    dateStrings: true, // Garante que os timestamps sejam tratados como strings, preservando o fuso horário
-  },
+const conexao = new Sequelize({
+  dialect: "sqlite",
+  storage: ":memory:",
   logging: false,
-  dialectModule: pg,
 });
+
+// esse é so pra quando tiver postgres instalado
+
+// const conexao = new Sequelize(process.env.DATABASE_URL, {
+//   host: "localhost",
+//   dialect: "postgres",
+//   timezone: "-03:00", // Fuso horário de Brasília (sem horário de verão)
+//   dialectOptions: {
+//     useUTC: false, // Evita usar UTC no PostgreSQL
+//     dateStrings: true, // Garante que os timestamps sejam tratados como strings, preservando o fuso horário
+//   },
+//   logging: false,
+//   dialectModule: pg,
+// });
 
 try {
   conexao.authenticate();
@@ -21,3 +30,5 @@ try {
 }
 
 module.exports = conexao;
+
+
