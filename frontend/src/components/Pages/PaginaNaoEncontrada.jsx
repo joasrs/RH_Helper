335ffe3bdcc81a18eb7fa4b31bbc906e813bf992
joas <joas_rsouza@outlook.com>
2025-06-22 17:59:33 -0300

    corrigido estilo pagina 404
    corrigido estilo pagina 404

--- frontend/src/components/Pages/PaginaNaoEncontrada.jsx
+++ frontend/src/components/Pages/PaginaNaoEncontrada.jsx
@@ -1,6 +1,6 @@
 export default function PaginaNaoEncontrada(){
     return (
-        <div className="principal">
+        <div className="principal" style={{margin: "auto"}}>
             <h1 style={{fontSize: 5.5 + "em", color: "burlywood"}}>404</h1>
             <h3>Página não encontrada!</h3>
             <h4>Clique <a href="/" style={{textDecoration: "underline"}}>aqui</a> para voltar para a página principal</h4>
